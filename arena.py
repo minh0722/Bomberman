d8fad50f167943175dcdfa1baecd5b74eae75e29
Minh Nguyen <minh0722@yahoo.com>
2015-06-23 13:11:33 +0300

    added a fewmore  methods for arena

--- arena.py
+++ arena.py
@@ -3,5 +3,13 @@ from util import Drawable
 
 class Arena(Drawable):
 	def __init__(self):
+		Drawable.__init__(self)
+
+	def draw(self, sprite):
 		pass
-	
+
+	def place_bomb(self, position):
+		pass
+
+	def _can_place_bomb(self, position):
+		pass
