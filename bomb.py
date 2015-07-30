fd7c983d2d73ee5e983be86ace55bd97a77e7062
Minh Nguyen <minh0722@yahoo.com>
2015-07-30 22:37:36 +0300

    add more bomb logic and cleared redundant codes in test

--- bomb.py
+++ bomb.py
@@ -1,6 +1,22 @@
 from object import Object
+from game_settings import *
 
 
 class Bomb(Object):
-	def __init__(self, x, y):
-		Object.__init__(self, x, y)
+    def __init__(self, x, y, bomb_range):
+        Object.__init__(self, x, y)
+
+        self.range = bomb_range
+        self.state = 'TICKING'
+        
+        self.bomb_sprite = PygAnimation([
+            ("resources/battle_tiles/battle_stage_1/normal_bomb_1.png", BOMB_FRAME_DURATION),
+            ("resources/battle_tiles/battle_stage_1/normal_bomb_2.png", BOMB_FRAME_DURATION),
+            ("resources/battle_tiles/battle_stage_1/normal_bomb_3.png", BOMB_FRAME_DURATION),
+            ("resources/battle_tiles/battle_stage_1/normal_bomb_4.png", BOMB_FRAME_DURATION)
+            ])
+
+        
+
+    def draw(self, game_display):
+        pass
--- test.py
+++ test.py
@@ -21,48 +21,6 @@ gameExit = False
 
 clock = pygame.time.Clock()
 
-x_move = 0
-y_move = 0
-
-x_change = 0
-y_change = 0
-
-frame_duration = 0.2
-player_down = PygAnimation([
-    ("resources/characters/bbm_front1.png", frame_duration),
-    ("resources/characters/bbm_front2.png", frame_duration),
-    ("resources/characters/bbm_front1.png", frame_duration),
-    ("resources/characters/bbm_front3.png", frame_duration)
-    ])
-player_up = PygAnimation([
-    ("resources/characters/bbm_back1.png", frame_duration),
-    ("resources/characters/bbm_back2.png", frame_duration),
-    ("resources/characters/bbm_back1.png", frame_duration),
-    ("resources/characters/bbm_back3.png", frame_duration),
-    ])
-player_left = PygAnimation([
-    ("resources/characters/bbm_left1.png", frame_duration),
-    ("resources/characters/bbm_left2.png", frame_duration),
-    ("resources/characters/bbm_left1.png", frame_duration),
-    ("resources/characters/bbm_left3.png", frame_duration),
-    ])
-player_right = PygAnimation([
-    ("resources/characters/bbm_right1.png", frame_duration),
-    ("resources/characters/bbm_right2.png", frame_duration),
-    ("resources/characters/bbm_right1.png", frame_duration),
-    ("resources/characters/bbm_right3.png", frame_duration),
-    ])
-
-conductor = PygConductor(player_down, player_up, player_left, player_right)
-
-# initially animation are in stopped state so nothing will be drawn
-# so we call pause so it will be drawn
-conductor.pause()
-
-current_direction = "down"
-
-p = Physics()
-
 arena = Arena()
 p1 = Player(22, 0, arena)
 input_handler = InputHandler(p1)
@@ -70,10 +28,6 @@ input_handler = InputHandler(p1)
 while not gameExit:
     arena.draw(game_display)
     p1.draw(game_display)
-
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         gameExit = True
 
     input_handler.handle_input()
 
